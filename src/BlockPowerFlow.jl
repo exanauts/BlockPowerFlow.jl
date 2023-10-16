98b30f44f907755c22fb9226753084788066d8bf
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-10-16 17:29:49 -0500

    Add ic(0) and ilu(0) preconditioners for Nvidia and AMD GPUs

--- src/BlockPowerFlow.jl
+++ src/BlockPowerFlow.jl
@@ -10,7 +10,16 @@ using CUDA
 using CUDA.CUSPARSE
 using CUDA.CUSOLVER
 
+using AMDGPU
+using AMDGPU.rocSPARSE
+using AMDGPU.rocSOLVER
+
+import LinearAlgebra.ldiv!
+
 export CUSOLVERRF
+
+include("preconditioners_nvidia.jl")
+include("preconditioners_amd.jl")
 
 include("block_utils.jl")
 include("block_bicgstab.jl")
